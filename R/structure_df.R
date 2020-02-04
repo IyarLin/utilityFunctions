de28afeb28282d5699ef636a3c40205eaf64a850
ilin <iyar.lin@viasat.com>
2020-02-04 13:51:43 +0200

    added date to structure_df, applied styler

--- R/structure_df.R
+++ R/structure_df.R
@@ -14,52 +14,70 @@
 #'   4. missing: Percent missing values
 #' @export
 
-structure_df <- function(data, max_string_length = 60){
-  tab <- lapply(data, function(column){
-    if(class(column)[1] == "numeric" | class(column) == "integer"){
+structure_df <- function(data, max_string_length = 60) {
+  tab <- lapply(data, function(column) {
+    if (class(column)[1] == "numeric" | class(column) == "integer") {
       unique_values <- summary(column)[1:6]
       zeros <- str_extract(unique_values, "0\\.0+")
       zeros[is.na(zeros)] <- "00"
-      zeros <- max(mean(nchar(zeros)-2), 2)
+      zeros <- max(mean(nchar(zeros) - 2), 2)
       sample_values <- paste0(paste(names(unique_values), round(unname(unique_values), zeros), sep = " = "), collapse = ", ")
-      ans <- data.frame(class = class(column),
-                        sample_values = sample_values,
-                        missing = paste0(round(mean(is.na(column))*100, 2), "%"),
-                        stringsAsFactors = F)
-    } else if(class(column)[1] == "factor" | class(column)[1] == "ordered"){
+      ans <- data.frame(
+        class = class(column),
+        sample_values = sample_values,
+        missing = paste0(round(mean(is.na(column)) * 100, 2), "%"),
+        stringsAsFactors = F
+      )
+    } else if (class(column)[1] == "factor" | class(column)[1] == "ordered") {
       unique_values <- unique(na.omit(column))
       n_unique_values <- length(unique_values)
       sample_values <- paste0(sample(unique_values, size = min(5, n_unique_values), replace = F), collapse = ", ")
-      if(nchar(sample_values) > max_string_length){
+      if (nchar(sample_values) > max_string_length) {
         sample_values <- paste0(substr(sample_values, 1, max_string_length - 3), "...")
       }
-      ans <- data.frame(class = paste0("factor with ", n_unique_values, " levels"),
-                        sample_values = sample_values,
-                        missing = paste0(round(mean(is.na(column))*100, 2), "%"),
-                        stringsAsFactors = F)
-    } else if(class(column)[1] == "character") {
+      ans <- data.frame(
+        class = paste0("factor with ", n_unique_values, " levels"),
+        sample_values = sample_values,
+        missing = paste0(round(mean(is.na(column)) * 100, 2), "%"),
+        stringsAsFactors = F
+      )
+    } else if (class(column)[1] == "character") {
       unique_values <- unique(na.omit(column))
       n_unique_values <- length(unique_values)
       sample_values <- paste0(sample(unique_values, size = min(5, n_unique_values), replace = F), collapse = ", ")
-      if(nchar(sample_values) > max_string_length){
+      if (nchar(sample_values) > max_string_length) {
         sample_values <- paste0(substr(sample_values, 1, max_string_length - 3), "...")
       }
-      ans <- data.frame(class = paste0("char with ", n_unique_values, " unique values"),
-                        sample_values = sample_values,
-                        missing = paste0(round(mean(is.na(column)), 2)*100, "%"),
-                        stringsAsFactors = F)
-    } else if(class(column)[1] == "logical"){
-      ans <- data.frame(class = "logical",
-                        sample_values = paste0("FALSE freq = ", round(sum(column == FALSE, na.rm = T)/length(column),3), ", ",
-                                               "TRUE freq = ", round(sum(column == TRUE, na.rm = T)/length(column),3)),
-                        missing = paste0(round(mean(is.na(column))*100, 2), "%"),
-                        stringsAsFactors = F)
-
+      ans <- data.frame(
+        class = paste0("char with ", n_unique_values, " unique values"),
+        sample_values = sample_values,
+        missing = paste0(round(mean(is.na(column)), 2) * 100, "%"),
+        stringsAsFactors = F
+      )
+    } else if (class(column)[1] == "logical") {
+      ans <- data.frame(
+        class = "logical",
+        sample_values = paste0(
+          "FALSE freq = ", round(sum(column == FALSE, na.rm = T) / length(column), 3), ", ",
+          "TRUE freq = ", round(sum(column == TRUE, na.rm = T) / length(column), 3)
+        ),
+        missing = paste0(round(mean(is.na(column)) * 100, 2), "%"),
+        stringsAsFactors = F
+      )
+    } else if (class(column)[1] == "Date") {
+      sample_values <- as.character(as.Date(as.vector(summary.Date(column)[1:6]), origin = "1970-01-01"))
+      ans <- data.frame(
+        class = "Date",
+        sample_values = paste0(sample_values, collapse = ", "),
+        missing = paste0(round(mean(is.na(column)) * 100, 2), "%"),
+        stringsAsFactors = F
+      )
     }
     return(ans)
   })
   tab <- bind_rows(tab)
-  tab <- tab %>% mutate(variable = names(data)) %>%
+  tab <- tab %>%
+    mutate(variable = names(data)) %>%
     select(variable, everything())
   return(tab)
 }
