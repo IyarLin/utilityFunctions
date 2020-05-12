5744ef40544fc2c74e58c25d2bad0157be8a8c5a
ilin <iyar.lin@viasat.com>
2020-05-12 11:04:32 +0300

    added small option to merge levels

--- R/merge_small_factor_levels.R
+++ R/merge_small_factor_levels.R
@@ -1,19 +1,24 @@
 #' @title Merge small factor levels.
-#' @description Given an input factor/character vector,
+#' @description given an input factor/character vector,
 #'   merges together all vector levels that account for
 #'   less than \code{min.perc} of the total number of
 #'   elements in the vector
-#' @param x Either character of factor vector
-#' @return Factor vector, with small levels merged
+#' @param x either character of factor vector
+#' @param min.perc minimum frequency of non NA values for a factor level not to be merged
+#' @param case_weights case weights used in calculation of level frequency
+#' @param good_levels should syntactically valid levels be required?
+#' @return factor vector, with small levels merged
 #' @export
 
-merge_small_factor_levels <- function(x, min.perc = 0.01, case_weights = NULL) {
+merge_small_factor_levels <- function(x, min.perc = 0.01, case_weights = NULL, good_levels = T) {
   # min.perc = minimum percentage of non NA values
   if (!class(x) %in% c("character", "factor")) stop("input vector class should be either character or factor")
   if (class(x) == "character") x <- factor(x)
   if (is.null(case_weights)) case_weights <- rep(1, length(x))
   good_levels <- make.names(levels(x))
-  if (!identical(good_levels, levels(x))) stop("Levels of input variable do not make good names. Consider running make.names on that variable prior to feeding it to this function")
+  if (!identical(good_levels, levels(x))) stop("Levels of input variable do not make good names.
+                                               Either set good_levels = F or consider running make.names
+                                               on that variable prior to feeding it to this function")
   x_lev <- sort(tapply(case_weights, x, sum))
   x_lev <- x_lev / sum(x_lev)
   n_merge <- sum(x_lev < min.perc)
